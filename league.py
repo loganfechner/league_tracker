c3e0528afd7974ee9d3c938f2704909e0be70ec0
Logan Fechner <lmfechner158@gmail.com>
2020-07-14 02:28:38 -0700

    Update league.py
    new main function plust token tracking implementation

--- league.py
+++ league.py
@@ -26,9 +26,50 @@ def disparity_check():
     temp_string = ','.join(uncomplete_champs)
     champion_file.write(temp_string)
 
+def champ_tokens6():
+    token_file = open("champ_tokens6.txt", 'r')
+    temp_file = token_file.read()
+    token_list = temp_file.split(',')
+    return(token_list)
+
+def champ_tokens7():
+    token_file = open("champ_tokens7.txt", 'r')
+    temp_file = token_file.read()
+    token_list = temp_file.split(',')
+    return(token_list)
+
+
 
 def champ_eternals():
-    
-#print(uncomplete_champ_list())
-#print(complete_champ_list())
-disparity_check()
+    return()
+
+def main():
+    lvl7count = 0
+    for i in complete_champ_list():
+        lvl7count += 1
+
+    lvl7tokens = 0
+    for i in champ_tokens7():
+        lvl7tokens += 1
+    lvl6tokens = 0
+    for i in champ_tokens6():
+        lvl6tokens += 1
+
+    print(f"You currently have {lvl7count} champions level 7.")
+    print(f"You currently have {lvl6tokens} mastery 6 champion tokens")
+    print("You need to play:")
+    lvl6tokensnodup = champ_tokens6()
+    lvl6tokensnodup = list(dict.fromkeys(lvl6tokensnodup))
+    for i in lvl6tokensnodup:
+        print(i)
+    print(f"You currently have {lvl7tokens} mastery 7 champion tokens.")
+    print("You need to play:")
+    lvl7tokensnodup = champ_tokens7()
+    lvl7tokensnodup = list(dict.fromkeys(lvl7tokensnodup))
+    for i in lvl7tokensnodup:
+        print(i)
+
+
+
+
+main()
