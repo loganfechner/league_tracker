ddd6ce98495d730d8097b8129cbe41dadf3d38ef
Logan Fechner <lmfechner158@gmail.com>
2020-07-14 02:37:01 -0700

    Update league.py
    added comments above each function

--- league.py
+++ league.py
@@ -1,14 +1,16 @@
+#reads the text file to detrmine which champs are not lvl 7 and outputs as a list
 def uncomplete_champ_list():
     champion_file = open("uncomplete_champs.txt", 'r')
     temp_file = champion_file.read()
     return(temp_file.split(','))
+#reads the text file to detrmine which champs are lvl 7 and outputs as a list
 def complete_champ_list():
     champion_file = open("complete_champs.txt", 'r')
     temp_file = champion_file.read()
     return(temp_file.split(','))
 
 
-
+#takes the two lists from above and compares them against one another to see which champions need to be removed from the uncompleted champions list this then writes to the uncompleted champions file and updates it
 def disparity_check():
     temp1 = ''
     temp2 = ''
@@ -26,23 +28,25 @@ def disparity_check():
     temp_string = ','.join(uncomplete_champs)
     champion_file.write(temp_string)
 
+#creates a list of level 6 mastery tokens
 def champ_tokens6():
     token_file = open("champ_tokens6.txt", 'r')
     temp_file = token_file.read()
     token_list = temp_file.split(',')
     return(token_list)
-
+#creates a list of level 7 mastery tokens
 def champ_tokens7():
     token_file = open("champ_tokens7.txt", 'r')
     temp_file = token_file.read()
     token_list = temp_file.split(',')
     return(token_list)
 
-
-
+## IDEA: track eternals
 def champ_eternals():
     return()
 
+
+#main function that will output all of the data in a readable understandble manner
 def main():
     lvl7count = 0
     for i in complete_champ_list():
@@ -71,5 +75,5 @@ def main():
 
 
 
-
+#calls the main function
 main()
